f9a543129f198cea7ccc2460a5be76a6be91beee
PosseCat <vermilmoon@gmail.com>
2017-08-30 19:20:55 +0800

    #8.30 last

--- src/components/routes.js
+++ src/components/routes.js
@@ -3,8 +3,5 @@ import { Link } from 'react-router-dom'
 import { Menu, Icon } from 'antd'
 
 import UnitAdd from './routes/unitAdd'
-import GoodsAdd from './components/Goods/goodsAdd'
-import CateList from './components/Cate/cateList'
-import CateAdd from './components/Cate/cateAdd'
 
 export {UnitAdd}
